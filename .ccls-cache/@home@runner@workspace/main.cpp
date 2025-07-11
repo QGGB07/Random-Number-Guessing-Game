05d81365bef24c9d871ebcd9df961d8aaf4489d7
QGGB07 <qgb23300@email.vccs.edu>
2025-07-11 00:11:21 +0000

    Working on code

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,3 +1,37 @@
 #include <iostream>
+#include <cstdlib>   // for rand() and srand()
+#include <ctime>     // for time()
 
-int main() { std::cout << "Hello World!\n"; }
+using namespace std;
+
+int main() {
+    // Seed the random number generator
+    srand(time(0));
+
+    // Generate a random number between 1 and 100
+    int randomNumber = rand() % 100 + 1;
+    int guess;
+    int guessCount = 0;
+
+    cout << "Welcome to the Random Number Guessing Game!" << endl;
+    cout << "I'm thinking of a number between 1 and 100." << endl;
+
+    // Loop until the user guesses the correct number
+    do {
+        cout << "Enter your guess: ";
+        cin >> guess;
+        guessCount++;
+
+        if (guess > randomNumber) {
+            cout << "Too high, try again." << endl;
+        } else if (guess < randomNumber) {
+            cout << "Too low, try again." << endl;
+        } else {
+            cout << "Congratulations! You guessed the number in " 
+                 << guessCount << " attempts." << endl;
+        }
+
+    } while (guess != randomNumber);
+
+    return 0;
+}
